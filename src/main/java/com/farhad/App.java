6cfabc89a4b43e7799326074d513385b02975050
ferhad2207 <frhdmehdiyev@gmail.com>
2021-05-14 11:45:36 +0400

    new maven dependencies are added

---
+++ src/main/java/com/farhad/App.java
@@ -0,0 +1,58 @@
+package com.farhad;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
+import org.kordamp.bootstrapfx.scene.layout.Panel;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+//        Panel panel = new Panel("This is the title");
+//        panel.getStyleClass().add("panel-primary");                            //(2)
+//        BorderPane content = new BorderPane();
+//        content.setPadding(new Insets(20));
+//        Button button = new Button("Hello BootstrapFX");
+//        button.getStyleClass().setAll("btn","btn-danger");                     //(2)
+//        content.setCenter(button);
+//        panel.setBody(content);
+//
+//        Scene scene = new Scene(panel);
+//        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());       //(3)
+//
+//        primaryStage.setTitle("BootstrapFX");
+//        primaryStage.setScene(scene);
+//        primaryStage.sizeToScene();
+//        primaryStage.show();
+
+        scene = new Scene(loadFXML("view/primary"), 640, 480);
+        scene.getStylesheets().add(String.valueOf(getClass().getResource("style/buttons.css")));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module com.farhad {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.jfoenix;
+    requires org.kordamp.bootstrapfx.core;
 
     exports com.farhad.controllers;
     opens com.farhad.controllers to javafx.fxml;
-    exports com.farhad.application;
-    opens com.farhad.application to javafx.fxml;
+    exports com.farhad;
+    opens com.farhad to javafx.fxml;
 }
