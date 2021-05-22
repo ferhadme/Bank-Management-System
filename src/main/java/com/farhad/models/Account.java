67629d4bac5f8d6c91ef4b70ebac09a637771680
ferhad2207 <frhdmehdiyev@gmail.com>
2021-05-22 16:30:03 +0400

    account model is implemented

--- src/main/java/com/farhad/models/Account.java
+++ src/main/java/com/farhad/models/Account.java
@@ -1,5 +1,59 @@
 package com.farhad.models;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.StringProperty;
+
 public class Account {
-//    private
+    private StringProperty accountId;
+    private StringProperty accountName;
+    private StringProperty otherAccountDetails;
+    private DoubleProperty amountOfMoney;
+
+    public String getAccountId() {
+        return accountId.get();
+    }
+
+    public StringProperty accountIdProperty() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId.set(accountId);
+    }
+
+    public String getAccountName() {
+        return accountName.get();
+    }
+
+    public StringProperty accountNameProperty() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName.set(accountName);
+    }
+
+    public String getOtherAccountDetails() {
+        return otherAccountDetails.get();
+    }
+
+    public StringProperty otherAccountDetailsProperty() {
+        return otherAccountDetails;
+    }
+
+    public void setOtherAccountDetails(String otherAccountDetails) {
+        this.otherAccountDetails.set(otherAccountDetails);
+    }
+
+    public double getAmountOfMoney() {
+        return amountOfMoney.get();
+    }
+
+    public DoubleProperty amountOfMoneyProperty() {
+        return amountOfMoney;
+    }
+
+    public void setAmountOfMoney(double amountOfMoney) {
+        this.amountOfMoney.set(amountOfMoney);
+    }
 }
